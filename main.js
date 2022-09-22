11719a5f36d266eddd854df84759b5b3ccd7919b
alex20355 <103307773+alex20355@users.noreply.github.com>
2022-09-22 15:02:30 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-/*
-# startwars_application
-### prenom: Alexandre                                       #                                 
-### nom: Djetic                                             #
-### date: 22/09/2022                                        # 
-### version: 1.0.0                                          # 
-### license: github                                         #  
-this web application deliver information of people that appair in starwars. 
-Also, information about people are store in a file of format json package,you can find the original database from this api : https://akabab.github.io/starwars-api/  the full file   
-
-this application got two break point:  
-- from 0 px to 700 px, this rule deserve for mobile and low size screen  
-- from 700 px and more, this rule deserve for computer, desktop and tablette that have more than 10 inch  
-
-note:     
-- this application will use this api: https://akabab.github.io/starwars-api/ in the next update     
-- I have also some idea to add a quiz, which got theme starwars about actor, people that appair in side story serie.  
-
-disclamer:  this application is no mean for commercial use
-*/
-
 let passage_personnage = document.querySelector("#passer")
 let info = document.querySelector("#info")
 let footer = document.querySelector("#piedpage")
